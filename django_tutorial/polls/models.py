79ee78762a4cfec5f3db6362e859fcabb336225e
tanisyu <tanisyu1997@gmail.com>
2021-04-10 11:32:48 +0900

    add class method and change __str__ method

--- django_tutorial/polls/models.py
+++ django_tutorial/polls/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,6 +10,9 @@ class Question(models.Model):
     def __str__(self):
       return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -15,4 +20,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-      return self.choice_text
+      return '%s, %s' % (self.choice_text, self.votes)
